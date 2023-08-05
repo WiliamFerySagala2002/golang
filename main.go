5e46a849f3a074ad2047ea2736f5b4f54caa9fe9
WiliamFerySagala2002 <sagalawiliam0@gmail.com>
2023-08-05 17:43:59 +0700

    Variable

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	var firstName string = "john"
 	lastName = "dea"
 	fmt.Println(lastName, firstName)
+
+	fullName := [3]string{"Wiliam", "Fery", "Sagala",}
+	fmt.Println(fullName)
 }
 
